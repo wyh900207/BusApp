7d5acfe8682db0d3563974d09ba6c878e9b9fe4a
HomerLynn <homerlynn@HomerLynndeiMac.local>
2017-10-26 16:54:37 +0800

    init project

---
+++ BusApp/BusApp/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusApp
+//
+//  Created by HomerLynn on 2017/10/26.
+//  Copyright © 2017年 HomerLynn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
